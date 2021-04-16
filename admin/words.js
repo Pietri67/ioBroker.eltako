215d2f80951c5c27fa4ff3423a7b0885607588eb
Pietri67 <torsten.voigt@nobibum.de>
2021-04-16 16:37:21 +0200

    setup remote debugging

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'eltako adapter settings': {
+		'en': 'Adapter settings for eltako',
+		'de': 'Adaptereinstellungen für eltako',
+		'ru': 'Настройки адаптера для eltako',
+		'pt': 'Configurações do adaptador para eltako',
+		'nl': 'Adapterinstellingen voor eltako',
+		'fr': "Paramètres d'adaptateur pour eltako",
+		'it': "Impostazioni dell'adattatore per eltako",
+		'es': 'Ajustes del adaptador para eltako',
+		'pl': 'Ustawienia adaptera dla eltako',
+		'zh-cn': 'eltako的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
