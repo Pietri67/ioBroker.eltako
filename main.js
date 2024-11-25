5ced58f376bacb9a8cbca3be548f563c3e045600
Torsten Voigt <voigt@MBPvonTorsten5.atvoigt.local>
2024-11-25 12:57:20 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,142 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v2.6.5
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+const SerialPort = require('serialport');
+const ByteLength = require('@serialport/parser-byte-length');
+
+// Communication Port/Parser
+let commPort = null;
+let commParser = null;
+
+
+class Eltako extends utils.Adapter {
+
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: 'eltako',
+		});
+		this.on('ready', this.onReady.bind(this));
+		this.on('stateChange', this.onStateChange.bind(this));
+		this.on('unload', this.onUnload.bind(this));
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		// Initialize your adapter here
+		// Reset the connection indicator during startup
+		await this.setStateAsync('info.connection', false, true);
+
+		// try to initialize communication
+		if (this.config.usbport) {
+
+			// create serial port
+			commPort = new SerialPort(this.config.usbport, {
+				baudRate: 57600
+			});
+
+			// create parser 14Byte
+			commParser = commPort.pipe(new ByteLength({length: 14}));
+
+			// initialize communication
+			await this.communication();
+		}
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			// stop communication
+			if (commPort != null) {
+				commPort.close();
+			}
+
+			// update connection state.
+			this.setState('info.connection', false, true);
+
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	/**
+	 * Is called if a subscribed state changes
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	onStateChange(id, state) {
+		if (state) {
+			// The state was changed
+			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		} else {
+			// The state was deleted
+			this.log.info(`state ${id} deleted`);
+		}
+	}
+
+	/**
+	 *   serial communiation eltako
+	 */
+	async communication() {
+
+		// port opened
+		commPort.on('open', () => {
+
+			// setup parser
+			commParser.on('data' , (data) => {
+			});
+
+			// update connection state.
+			this.setState('info.connection', true, true);
+
+			// Logfile
+			this.log.info('Eltako usb/serial port ' + this.config.usbport + ' with baudrate ' + this.config.baudrate + ' opened.');
+		});
+
+		// port closed
+		commPort.on('close', () => {
+			// update connection state.
+			this.setState('info.connection', false, true);
+
+			// Logfile
+			this.log.info('Eltako usb/serial port ' + this.config.usbport + ' closed.');
+		});
+
+		// port error
+		commPort.on('error', (error) => {
+			// Logfile
+			this.log.info('Eltako usb/serial port ' + this.config.usbport + ' error: ' + error);
+		});
+	}
+
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Eltako(options);
+} else {
+	// otherwise start the instance directly
+	new Eltako();
+}
